f24c4264708d41538753ee0f02e5b7b8da4b3fb7
Phyllis Huang <fillycream@gmail.com>
2023-04-07 09:19:43 -0400

    Instantiate vase, flowers and sandwich when their respective menu buttons are clicked. Instantiate positions and size still not yet adjusted

---
+++ VRTut/Assets/Scripts/instanGameObj.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class instanGameObj : MonoBehaviour
+{
+    public GameObject objectPrefab;
+
+    public void CreateObj()
+    {
+        GameObject newObj = Instantiate(objectPrefab);
+    }
+}
